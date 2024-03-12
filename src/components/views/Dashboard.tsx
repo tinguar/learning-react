e19c66fe4f765ed3fd523e1ad7cea44df569563c
baguvix23 <jordymazac@gmail.com>
2024-03-12 11:58:57 -0500

    feat: Add new 'Dashboard' component

--- src/components/views/Dashboard.tsx
+++ src/components/views/Dashboard.tsx
@@ -1,52 +1,65 @@
-import React from 'react'
-
 function Dashboard() {
-
-   const data=[
+  const data = [
     {
-        id:1,
-        title:"some task",
-        description:"some description",
-        status:{
-            label:"done",
-            value:"done"
-        },
-        difficulty:"easy"
+      id: 1,
+      title: "some task 1",
+      description: "some description 1",
+      status: {
+        label: "done",
+        value: "done",
+      },
+      difficulty: "easy",
     },
     {
-        id:2,
-        title:"some task",
-        description:"some description",
-        status:{
-            label:"done",
-            value:"done"
-        },
-        difficulty:"easy"
+      id: 2,
+      title: "some task 2",
+      description: "some description 2",
+      status: {
+        label: "done",
+        value: "done",
+      },
+      difficulty: "medium",
     },
     {
-        id:3,
-        title:"some task",
-        description:"some description",
-        status:{
-            label:"done",
-            value:"done"
-        },
-        difficulty:"easy"
-    }
-   ]
+      id: 3,
+      title: "some task 3",
+      description: "some description 3",
+      status: {
+        label: "done",
+        value: "done",
+      },
+      difficulty: "hard",
+    },
+  ];
 
   return (
-    <div>Dashboard
-
-        {
-        /*  Alejandro:
-                - Crear una tabla "Crear un componente de fila de tabla" 
-                - Prop de "data"
-                - Crear la fila de la tabla como un componente de tipo atom.
-        */
-        }
+    <div>
+      <h1>Dashboard</h1>
+      <table className="table table-striped">
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th>Status</th>
+            <th>Difficulty</th>
+          </tr>
+        </thead>
+        {/* Utilize el metodo map en el arreglo de data para iterar para cada objeto */}
+        <tbody>
+          {data.map((item) => (
+            <tr key={item.id}>
+              <td>{item.id}</td>
+              <td>{item.title}</td>
+              <td>{item.description}</td>
+              <td>{item.status.label}</td>
+              <td>{item.difficulty}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
 
-export default Dashboard
+export default Dashboard;
